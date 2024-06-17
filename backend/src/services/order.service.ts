b014075b4efe19fda01da3ca2fe051dcdbf546a5
Luís Paulo Silva Trevisan <lpst@cin.ufpe.br>
2024-06-17 05:26:44 -0300

    feat: estimed-delivery-time

--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -2,6 +2,8 @@ import OrderModel from '../models/order.model';
 import OrderRepository from '../repositories/order.repository';
 import MenuRepository from '../repositories/menu.repository';
 import OrderEntity from '../entities/order.entity';
+import cep from 'cep-promise'; //duvida da API
+import axios from 'axios';
 
 class OrderService {
   private orderRepository: OrderRepository;
@@ -43,6 +45,36 @@ class OrderService {
       ...updatedOrder,
       items: menu.filter(el => updatedOrder.itemsId.includes(el.id)),
     });
+  }
+
+  private async calculateDeliveryTime(cepValue: string): Promise<number> {
+    const cepResult = await cep(cepValue);
+
+    if (!cepResult || !cepResult.street || !cepResult.neighborhood || !cepResult.city || !cepResult.state) {
+      throw new Error('Endereço não encontrado para o CEP fornecido.');
+    }
+
+    const address = `${cepResult.street} ${cepResult.neighborhood} ${cepResult.city}, ${cepResult.state}`;
+
+    const googleApiKey = 'AIzaSyDt2t8-Q1LYyEC37XykoWq5pOJohzV4RG4';
+    const response = await axios.get('https://maps.googleapis.com/maps/api/distancematrix/json', {
+      params: {
+        destinations: address,
+        origins: 'Washington, DC, USA', // Substituir pela origem real
+        key: googleApiKey,
+      },
+    });
+
+    const distanceMatrixResult = response.data;
+
+    if (distanceMatrixResult.status !== 'OK' || distanceMatrixResult.rows[0].elements[0].status !== 'OK') {
+      throw new Error('Não foi possível calcular o tempo de entrega.');
+    }
+
+    // Obter a duração em segundos
+    const duration = distanceMatrixResult.rows[0].elements[0].duration.value;
+
+    return duration;
   }
 }
 
